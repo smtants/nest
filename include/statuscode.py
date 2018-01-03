011ae5d25e4e1e55f2db539524944e75cef6e644
smtants <smtants@outlook.com>
2018-01-03 17:11:19 +0800

    add statuscode

---
+++ include/statuscode.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# coding: utf8
+
+#
+#   status code
+#   xianwen.zhang
+#   2017-09-21
+#
+
+SUCCESS          = 0               #    success
+
+#   接口
+API_ABNORMA      = 601             #    abnormal interface
+
+#   请求
+REQ_PARAM_ERROR  = 701             #    request parameter error
+REQ_FORMAT_ERROR = 702             #    request format error
+
+#   数据操作
+DAT_INSERT_FAIL  = 801             #   data insert fail
+DAT_INSERT_EXIST = 802             #   data insert fail
+DAT_UPDATE_FAIL  = 811             #   data update fail
